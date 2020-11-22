96996952ea54d0812b470b9761819242a9c98d56
username <username@USUARIO>
2020-11-22 13:48:29 -0600

    Added MVVM main objects

--- PDFutilities/WpfApplication/ViewModel/MainViewModel.cs
+++ PDFutilities/WpfApplication/ViewModel/MainViewModel.cs
@@ -1,34 +1,29 @@
 using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
 
 namespace WpfApplication.ViewModel
 {
-    /// <summary>
-    /// This class contains properties that the main View can data bind to.
-    /// <para>
-    /// Use the <strong>mvvminpc</strong> snippet to add bindable properties to this ViewModel.
-    /// </para>
-    /// <para>
-    /// You can also use Blend to data bind with the tool's support.
-    /// </para>
-    /// <para>
-    /// See http://www.galasoft.ch/mvvm
-    /// </para>
-    /// </summary>
     public class MainViewModel : ViewModelBase
     {
-        /// <summary>
-        /// Initializes a new instance of the MainViewModel class.
-        /// </summary>
+        public string AppTitle { get; set; }
+        public ObservableCollection<File> FileList { get; set; }
+        public ICommand LoadFilesCommand { get; set; }
         public MainViewModel()
         {
-            ////if (IsInDesignMode)
-            ////{
-            ////    // Code runs in Blend --> create design time data.
-            ////}
-            ////else
-            ////{
-            ////    // Code runs "for real"
-            ////}
+            AppTitle = Constants.AppTitle;
+            LoadFilesCommand = new RelayCommand(LoadFilesMethod);
+        }
+
+        private void LoadFilesMethod()
+        {
+            FileList = new ObservableCollection<File>()
+            {
+                new File() { Name = "001.pdf" },
+                new File() { Name = "002.pdf" }
+            };
+            this.RaisePropertyChanged(() => this.FileList);
         }
     }
 }
