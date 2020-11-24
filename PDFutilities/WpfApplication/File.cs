6907b51fca32a8ac86a14ceac003977d90003e41
username <username@USUARIO>
2020-11-24 12:42:35 -0600

    Modified main view tu add real file data and pdf icon

--- PDFutilities/WpfApplication/File.cs
+++ PDFutilities/WpfApplication/File.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,5 +10,12 @@ namespace WpfApplication
     public class File
     {
         public string Name { get; set; }
+        public string FullPath { get; set; }
+
+        public File(string FullPath)
+        {
+            this.FullPath = FullPath;
+            this.Name = Path.GetFileName(FullPath);
+        }
     }
 }
--- PDFutilities/WpfApplication/ViewModel/MainViewModel.cs
+++ PDFutilities/WpfApplication/ViewModel/MainViewModel.cs
@@ -1,28 +1,52 @@
 using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.Command;
 using System.Collections.ObjectModel;
+using System.IO;
+using System.Windows;
+using System.Windows.Forms;
 using System.Windows.Input;
 
 namespace WpfApplication.ViewModel
 {
     public class MainViewModel : ViewModelBase
     {
+        public string Folder { get; set; }
         public string AppTitle { get; set; }
         public ObservableCollection<File> FileList { get; set; }
         public ICommand LoadFilesCommand { get; set; }
+        public ICommand SelectFolderCommand { get; set; }
         public MainViewModel()
         {
             AppTitle = Constants.AppTitle;
             LoadFilesCommand = new RelayCommand(LoadFilesMethod);
+            SelectFolderCommand = new RelayCommand(SelectFolderMethod);
+        }
+
+        private void SelectFolderMethod()
+        {
+            using (var dialog = new FolderBrowserDialog())
+            {
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    Folder = dialog.SelectedPath;
+                    this.RaisePropertyChanged(() => this.Folder);
+                }
+            }
         }
 
         private void LoadFilesMethod()
         {
-            FileList = new ObservableCollection<File>()
+            if (Directory.Exists(Folder))
             {
-                new File() { Name = "001.pdf" },
-                new File() { Name = "002.pdf" }
-            };
+                FileList = new ObservableCollection<File>();
+                var files = Directory.GetFiles(Folder, "*.pdf");
+
+                foreach (var file in files)
+                {
+                    FileList.Add(new File(file));
+                }
+            }
+
             this.RaisePropertyChanged(() => this.FileList);
         }
     }
