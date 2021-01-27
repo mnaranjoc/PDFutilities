eaf0cfd7660f9de3ccc9db48f9aaf38dc25d2ccd
Manuel Naranjo Contreras <manuelnaranjo@outlook.com>
2021-01-26 21:00:57 -0600

    Merge module created

---
+++ PDFutilities/PDFUtil.Module.Merge/Views/MergeView.xaml.cs
@@ -0,0 +1,30 @@
+﻿using PDFUtil.Module.Merge.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PDFUtil.Module.Merge.Views
+{
+    /// <summary>
+    /// Interaction logic for MergeView.xaml
+    /// </summary>
+    public partial class MergeView : UserControl
+    {
+        public MergeView(MergeViewModel vm)
+        {
+            InitializeComponent();
+            DataContext = vm;
+        }
+    }
+}
--- PDFutilities/PDFUtil.Module.Navigation/ViewModels/NavigationViewModel.cs
+++ PDFutilities/PDFUtil.Module.Navigation/ViewModels/NavigationViewModel.cs
@@ -1,20 +1,33 @@
-﻿using Prism.Modularity;
+﻿using Prism.Commands;
+using Prism.Modularity;
 using Prism.Regions;
+using System.Windows.Input;
 using Unity;
 
 namespace PDFUtil.Module.Navigation.ViewModels
 {
     public class NavigationViewModel
-    {
+    {        
         public IRegionManager RegionManager { get; set; }
         public IUnityContainer UnityContainer { get; set; }
         public IModuleManager ModuleManager { get; set; }
+        public DelegateCommand LoadMergeModuleCommand { get; private set; }
 
         public NavigationViewModel(IRegionManager regionManager, IModuleManager moduleManager, IUnityContainer container)
         {
             this.RegionManager = regionManager;
             this.ModuleManager = moduleManager;
             this.UnityContainer = container;
+
+            LoadMergeModuleCommand = new DelegateCommand(loadMergeModule);
+        }
+
+        private void loadMergeModule()
+        {
+            ModuleManager.LoadModule("MergeModule");
+            var contentRegion = RegionManager.Regions["ContentRegion"];
+            var newView = contentRegion.GetView("MergeView");
+            contentRegion.Activate(newView);
         }
     }
 }
